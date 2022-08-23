adbb8a0f103a5e6f2146cc93ecfb142e9ba35acf
enrique soteldo <soteldo18@hotmail.com>
2022-08-23 16:23:08 -0400

    boton logout

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,7 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import { LoginPage } from "../auth/pages/LoginPage"
-import { DcPage } from "../heroes/pages/DcPage"
-import { MarvelPage } from "../heroes/pages/MarvelPage"
+import { DcPage, MarvelPage } from "../heroes"
 import { Navbar } from "../ui"
 
 
