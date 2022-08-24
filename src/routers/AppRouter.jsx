242e8f3c45ca90f1c7c1ff8d12dbc1a94a0f81b5
Enrique <soteldo18@hotmail.com>
2022-08-23 22:19:26 -0400

    segunda ruta

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,18 +1,17 @@
 import { Navigate, Route, Routes } from "react-router-dom"
-import { LoginPage } from "../auth/pages/LoginPage"
-import { DcPage, MarvelPage } from "../heroes"
+import { LoginPage } from "../auth"
+import { DcPage, HeroesRoutes, MarvelPage } from "../heroes"
 import { Navbar } from "../ui"
 
 
 export const AppRouter = () => {
   return (
     <>
-    <Navbar/>
     <Routes>
-      <Route path="marvel" element={<MarvelPage />} />
-      <Route path="dc" element={<DcPage />} />
+      
       <Route path="login" element={<LoginPage />} />
-      <Route path="/" element={<Navigate to="/marvel" />} />
+       <Route path="/*" element={<HeroesRoutes />} />
+
     </Routes>
     </>
   )
