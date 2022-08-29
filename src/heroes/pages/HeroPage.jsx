d520759f4d0685eba321fe1a15cd1c6398bad7ba
enrique soteldo <soteldo18@hotmail.com>
2022-08-29 16:19:36 -0400

    busqueda heroes

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -33,12 +33,12 @@ export const HeroPage = () => {
       <div className="col-4">
         <img src={`/assets/heroes/${id}.jpg`}
          alt={hero.superhero}
-         className='img-thumbnail'
+         className='img-thumbnail animate__animated  animate__fadeInLeft'
          />
       </div>
-      <div className="col-8">
+      <div className="col-8 animate__animated animate__fadeInRight">
         <h3>{hero.superhero}</h3>
-        <ul className="list-group list-group-flush">
+        <ul className="list-group list-group-flush img-thumbnail ">
           <li className='list-group-item'> <b>Alter ego: </b>{hero.alter_ego}</li>
           <li className='list-group-item'> <b>Publisher: </b>{hero.publisher}</li>
           <li className='list-group-item'> <b>First Appearance: </b>{hero.first_appearance}</li>
--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,9 +1,59 @@
+import { useNavigate } from "react-router-dom";
+import { useForm } from "../../hooks/useForm"
 
 
 export const SearchPage = () => {
+const navigate=useNavigate();
+
+const {searchText,onInputChange}=useForm({
+  searchText:''
+});
+
+const onSearchSubmit=(event)=>{
+event.preventDefault();
+if(searchText.trim().length <=1 ) return;
+
+navigate(`?q=${searchText.toLowerCase().trim()}`);
+console.log({searchText});
+}
+
   return (
     <>
     <h1>SearchPage</h1>
+    <hr />
+    <div className="row">
+
+      <div className="col-5">
+      <h4>Searching</h4>
+      <hr />
+      <form onSubmit={onSearchSubmit}>
+        <input 
+        type="text"
+        placeholder="Search a Hero"
+        className="form-control"
+        name="searchText"
+        autoComplete="off"
+        value={searchText}
+        onChange={onInputChange}
+        />
+        <button
+        className="btn btn-outline-primary mt-1"
+        >
+          Search
+        </button>
+      </form>
+      </div>
+      <div className="col-7">
+        <h4>Results</h4>
+        <hr />
+        <div className="alert alert-primary">
+          Searh a hero
+        </div>
+        <div className="alert alert-danger">
+          not hero with <b>ABC</b>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
